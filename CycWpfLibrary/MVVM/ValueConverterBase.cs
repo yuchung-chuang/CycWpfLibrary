7d52d362346fd9d0b71deaed4bad51fffbdacf1c
Alex Chuang <1392alex1392@gmail.com>
2018-09-09 06:36:34 +0800

    Add ValueConverterBase and PointExtensions

---
+++ CycWpfLibrary/MVVM/ValueConverterBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CycWpfLibrary.MVVM
+{
+  public abstract class ValueConverterBase<T> : MarkupExtension, IValueConverter 
+    where T : class, new()
+  {
+    static T converter;
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+      return converter ?? (converter = new T());
+    }
+
+    public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+    public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+  }
+}
---
+++ CycWpfLibrary/NativeMethods/PointExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CycWpfLibrary.NativeMethods
+{
+  public static class PointExtensions
+  {
+    public static Point Add(this Point point1, Point point2)
+    {
+      return new Point(point1.X + point2.X, point1.Y + point2.Y);
+    }
+
+    public static Point Minus(this Point point1, Point point2)
+    {
+      return new Point(point1.X - point2.X, point1.Y - point2.Y);
+    }
+    
+  }
+}
