f84dccf30bd6c5dca688eb6b2d0a3567ec940606
Alex Chuang <1392alex1392@gmail.com>
2018-09-11 03:58:11 +0800

    Add ValueConverterBase

--- CycWpfLibrary/MVVM/ValueConverterBase.cs
+++ CycWpfLibrary/MVVM/ValueConverterBase.cs
@@ -9,13 +9,17 @@ using System.Windows.Markup;
 
 namespace CycWpfLibrary.MVVM
 {
-  public abstract class ValueConverterBase<T> : MarkupExtension, IValueConverter 
-    where T : class, new()
+  /// <summary>
+  /// 提供具有<see cref="IValueConverter"/>以及<see cref="MarkupExtension"/>功能的類別。
+  /// </summary>
+  /// <typeparam name="ValueConverterType">要實作的ValueConverter類別。</typeparam>
+  public abstract class ValueConverterBase<ValueConverterType> : MarkupExtension, IValueConverter 
+    where ValueConverterType : class, new()
   {
-    static T converter;
+    static ValueConverterType converter;
     public override object ProvideValue(IServiceProvider serviceProvider)
     {
-      return converter ?? (converter = new T());
+      return converter ?? (converter = new ValueConverterType());
     }
 
     public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
