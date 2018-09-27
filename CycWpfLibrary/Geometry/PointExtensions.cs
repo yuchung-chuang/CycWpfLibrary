a5aaf7ef32fa2ecc9653b9f1cd2a2077f10ccfdb
Alex Chuang <1392alex1392@gmail.com>
2018-09-27 02:25:12 +0100

    add DistanceTo and Norm in PointExtensions

--- CycWpfLibrary/Geometry/PointExtensions.cs
+++ CycWpfLibrary/Geometry/PointExtensions.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using static System.Math;
 
-namespace CycWpfLibrary.NativeMethods
+namespace CycWpfLibrary.Geometry
 {
   public static class PointExtensions
   {
+    public static double DistanceTo(this Point point1, Point point2)
+    {
+      return point1.Minus(point2).Norm();
+    }
+
+    public static double Norm(this Point point)
+    {
+      return Sqrt(Pow(point.X, 2) + Pow(point.Y, 2));
+    }
+
     public static Point Add(this Point point1, Point point2)
     {
       return new Point(point1.X + point2.X, point1.Y + point2.Y);
@@ -18,6 +29,6 @@ namespace CycWpfLibrary.NativeMethods
     {
       return new Point(point1.X - point2.X, point1.Y - point2.Y);
     }
-    
+
   }
 }
--- CycWpfLibrary/Linq/Expressions/ExpressionExtensions.cs
+++ CycWpfLibrary/Linq/Expressions/ExpressionExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,13 +14,23 @@ namespace CycWpfLibrary.Linq.Expressions
   public static class ExpressionExtensions
   {
     /// <summary>
-    /// 評估<paramref name="lambda"/>運算式並回傳結果。
+    /// 評估<see cref="LambdaExpression"/>並取得表達式的屬性值。
     /// </summary>
-    /// <typeparam name="T">回傳結果的類型。</typeparam>
-    /// <param name="lambda">要評估的運算式。</param>
-    public static T Evaluate<T>(this Expression<Func<T>> lambda)
+    public static T GetPropertyValue<T>(this Expression<Func<T>> lambda)
     {
       return lambda.Compile().Invoke();
+    }
+
+    public static void SetPropertyValue<T>(this Expression<Func<T>> lambda, T value)
+    {
+      //取得表達式
+      var expression = (lambda as LambdaExpression).Body as MemberExpression;
+      //取得表達式的屬性資訊
+      var propertyInfo = expression.Member as PropertyInfo;
+      //取得表達式左方的目標
+      var target = Expression.Lambda(expression.Expression).Compile().DynamicInvoke();
+
+      propertyInfo.SetValue(target, value);
     }
   }
 }
