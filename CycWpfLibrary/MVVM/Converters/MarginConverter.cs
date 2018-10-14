13112223f93044ce9c518182ea503e6f52050f58
Alex Chuang <1392alex1392@gmail.com>
2018-10-14 01:08:43 +0100

    Add Margin Converter and MathConverter

---
+++ CycWpfLibrary/MVVM/Converters/MarginConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace CycWpfLibrary.MVVM
+{
+  public class MarginConverter : MultiValueConverterBase<MarginConverter>
+  {
+    public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+      return new Thickness(System.Convert.ToDouble(values[0]),
+                           System.Convert.ToDouble(values[1]),
+                           System.Convert.ToDouble(values[2]),
+                           System.Convert.ToDouble(values[3]));
+    }
+
+    public override object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
+    {
+      return null;
+    }
+  }
+}
---
+++ CycWpfLibrary/MVVM/Converters/MultiConverterBase .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CycWpfLibrary.MVVM
+{
+  /// <summary>
+  /// 提供具有<see cref="IMultiValueConverter"/>以及<see cref="MarkupExtension"/>功能的類別。
+  /// </summary>
+  /// <typeparam name="MultiValueConverterType">要實作的MultiValueConverter類別。</typeparam>
+  public abstract class MultiValueConverterBase<MultiValueConverterType> : MarkupExtension, IMultiValueConverter 
+    where MultiValueConverterType : class, new()
+  {
+    static MultiValueConverterType converter;
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+      return converter ?? (converter = new MultiValueConverterType());
+    }
+
+    public abstract object Convert(object[] values, System.Type targetType, object parameter, CultureInfo culture);
+
+    public abstract object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture);
+  }
+}
