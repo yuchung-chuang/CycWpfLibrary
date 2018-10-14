9cccd13074646ab59db58e42b9cb9b34c0e67a25
Alex Chuang <1392alex1392@gmail.com>
2018-10-14 15:17:09 +0100

    DropImageReceiver

---
+++ CycWpfLibrary/Controls/DropImageReceiver.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace CycWpfLibrary.Controls
+{
+  /// <summary>
+  /// DropImageReceiver.xaml 的互動邏輯
+  /// </summary>
+  public partial class DropImageReceiver : UserControl
+  {
+    public DropImageReceiver()
+    {
+      InitializeComponent();
+    }
+
+    private void grid_Drop(object sender, DragEventArgs e)
+    {
+      if (e.Data.GetDataPresent(DataFormats.FileDrop))
+      {
+        // Note that you can have more than one file.
+        string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+        if (Path.GetExtension(files[0]))
+        {
+
+        } 
+
+        // Only care about the first file
+        image.Source = new BitmapImage(new Uri(files[0]));
+      }
+    }
+
+    private void grid_DragEnter(object sender, DragEventArgs e)
+    {
+
+    }
+
+    private void grid_DragLeave(object sender, DragEventArgs e)
+    {
+
+    }
+  }
+}
---
+++ CycWpfLibrary/Controls/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycWpfLibrary.Controls
+{
+  public static class Helpers
+  {
+    public static string ImageExtensionsString => string.Join(";", ImageExtensionsList);
+
+    public static List<string> ImageExtensionsList => ImageExtensionsLowerCase.Concat(ImageExtensionsUpperCase).ToList();
+
+    private static List<string> ImageExtensionsUpperCase => ImageExtensionsLowerCase.Select(s => s.ToUpper()).ToList();
+
+    private static readonly List<string> ImageExtensionsLowerCase = new List<string>
+    {
+      ".jpg",
+      ".bmp",
+      ".png",
+    };
+  }
+}
