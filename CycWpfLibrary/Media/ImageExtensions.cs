2883209ba5ff3e2fead9cf6fff707c9e9e7563f9
Alex Chuang <1392alex1392@gmail.com>
2018-10-15 01:11:16 +0100

    ImageExtensions

---
+++ CycWpfLibrary/Media/ImageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycWpfLibrary.Media
+{
+  public static class ImageExtensions
+  {
+    public static string String => "*" + string.Join(";*", List);
+
+    public static List<string> List => LowerCase.Concat(UpperCase).ToList();
+
+    private static List<string> UpperCase => LowerCase.Select(s => s.ToUpper()).ToList();
+
+    private static readonly List<string> LowerCase = new List<string>
+    {
+      ".jpg",
+      ".bmp",
+      ".png",
+    };
+  }
+}
