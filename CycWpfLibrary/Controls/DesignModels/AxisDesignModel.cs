04f3b4ea10632a86b4228156ab6cef3b85e1ec71
Alex Chuang <1392alex1392@gmail.com>
2018-10-28 03:40:00 +0000

    Add Axis Control

---
+++ CycWpfLibrary/Controls/DesignModels/AxisDesignModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycWpfLibrary.Controls
+{
+  public class AxisDesignModel : Axis
+  {
+    public static AxisDesignModel instance { get; private set; } = new AxisDesignModel();
+
+    public AxisDesignModel()
+    {
+      AxisLeft = 100;
+      AxisTop = 100;
+      AxisWidth = 150;
+      AxisHeight = 100;
+      AxisBrush = "Red";
+      ShadowBrush = "#99999999";
+    }
+  }
+}
