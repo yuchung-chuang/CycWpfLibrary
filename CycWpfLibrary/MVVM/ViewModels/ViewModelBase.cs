855b992adb7072bfd5d1251b17a3ca9b89477191
Alex Chuang <1392alex1392@gmail.com>
2018-10-30 02:28:54 +0000

    Modify ViewModelBase

--- CycWpfLibrary/MVVM/ViewModels/ViewModelBase.cs
+++ CycWpfLibrary/MVVM/ViewModels/ViewModelBase.cs
@@ -4,11 +4,16 @@ using System.Windows.Media.Imaging;
 namespace CycWpfLibrary.MVVM
 {
   /// <summary>
-  /// 提供實現了<see cref="INotifyPropertyChanged"/>介面的基底類別。
+  /// 提供實現了<see cref="INotifyPropertyChanged"/>介面以及ViewModel通用功能的基底類別。
   /// 請將ViewModel繼承自此類別，並使用Fody.PropertyChanged以連結View與ViewModel。
   /// </summary>
-  public class ViewModelBase : INotifyPropertyChanged
+  /// <typeparam name="ViewModelType">要繼承的ViewModel類型</typeparam>
+  public class ViewModelBase<ViewModelType> : INotifyPropertyChanged where ViewModelType : class, new()
   {
+    /// <summary>
+    /// ViewModel的靜態實例，提供Xaml引用。
+    /// </summary>
+    public static ViewModelType Instance { get; private set; } = new ViewModelType();
     /// <summary>
     /// 當其他屬性變更時觸發此事件。
     /// 請安裝Fody.PropertyChanged套件以自動呼叫此事件。
