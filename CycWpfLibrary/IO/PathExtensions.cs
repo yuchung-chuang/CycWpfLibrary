5de48bfaa6c7aa883cae1682616c30e93cd0f777
Alex Chuang <1392alex1392@gmail.com>
2018-11-16 02:36:39 +0000

    mere changes

--- CycWpfLibrary/IO/PathExtensions.cs
+++ CycWpfLibrary/IO/PathExtensions.cs
@@ -22,6 +22,14 @@ namespace CycWpfLibrary.IO
 
     public static string AbsolutePath(this string path) => Path.GetFullPath(path);
 
+    /// <summary>
+    /// 取得<paramref name="path"/>之<see cref="Uri"/>物件
+    /// </summary>
+    /// <param name="path">應用程式資料夾內之路徑</param>
+    public static Uri PackUri(this string path)
+    {
+      return new Uri($"pack://application:,,,/" + path);
+    }
 
   }
 }
