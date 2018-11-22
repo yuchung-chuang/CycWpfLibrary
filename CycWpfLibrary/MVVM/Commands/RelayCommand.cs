409fa7be242ece665eeeafae18c83cd25f057a43
Alex Chuang <1392alex1392@gmail.com>
2018-11-22 00:04:32 +0000

    add relaycommand<Tparam, Tresult>

--- CycWpfLibrary/MVVM/Commands/RelayCommand.cs
+++ CycWpfLibrary/MVVM/Commands/RelayCommand.cs
@@ -4,19 +4,10 @@ using System.Windows.Input;
 namespace CycWpfLibrary.MVVM
 {
   /// <summary>
-  /// 將方法封裝成命令的類別。
+  /// 將<see cref="Action"/>封裝成命令的類別。
   /// </summary>
-  public class RelayCommand : ICommand
+  public class RelayCommand : RelayCommandBase
   {
-    /// <summary>
-    /// 當<see cref="CanExecute(object)"/>改變時，觸發向此事件註冊過的方法
-    /// </summary>
-    public event EventHandler CanExecuteChanged
-    {
-      add { CommandManager.RequerySuggested += value; }
-      remove { CommandManager.RequerySuggested -= value; }
-    }
-
     private readonly Action _execute;
     private readonly Func<bool> _canExecute;
     /// <summary>
@@ -41,38 +32,28 @@ namespace CycWpfLibrary.MVVM
     /// <summary>
     /// 判斷<see cref="Execute(object)"/>是否可以執行。
     /// </summary>
-    public bool CanExecute(object parameter = null) => _canExecute is null || _canExecute();
+    public override bool CanExecute(object parameter = null) => _canExecute is null || _canExecute();
 
     /// <summary>
     /// 執行封裝的方法。
     /// </summary>
-    public void Execute(object parameter = null) => _execute();
+    public override void Execute(object parameter = null) => _execute();
   }
 
   /// <summary>
-  /// 將方法封裝成命令的泛型類別。
+  /// 將帶有參數的方法封裝成命令的類別。
   /// </summary>
-  /// <typeparam name="ParameterType"><see cref="Execute(object)"/>, <see cref="CanExecute(object)"/>的參數類型。</typeparam>
-  public class RelayCommand<ParameterType> : ICommand
+  /// <typeparam name="TParam">參數的類型。</typeparam>
+  public class RelayCommand<TParam> : RelayCommandBase
   {
-    /// <summary>
-    /// 當<see cref="CanExecute(object)"/>改變時，觸發向此事件註冊過的方法
-    /// </summary>
-    public event EventHandler CanExecuteChanged
-    {
-      add { CommandManager.RequerySuggested += value; }
-      remove { CommandManager.RequerySuggested -= value; }
-    }
-
-    private Predicate<ParameterType> _canExecute;
-    private Action<ParameterType> _execute;
-
+    private readonly Predicate<TParam> _canExecute;
+    private readonly Action<TParam> _execute;
 
     /// <summary>
     /// 將方法<paramref name="execute"/>封裝成命令，並使此命令永遠可執行。
     /// </summary>
     /// <param name="execute">要封裝成命令的方法，需有一個<see cref="object"/>類別的參數。</param>
-    public RelayCommand(Action<ParameterType> execute)
+    public RelayCommand(Action<TParam> execute)
     {
       this._execute = execute;
       _canExecute = (obj) => true;
@@ -82,7 +63,7 @@ namespace CycWpfLibrary.MVVM
     /// </summary>
     /// <param name="execute">要封裝成命令的方法，需有一個<see cref="object"/>類別的參數。</param>
     /// <param name="canExecute">判斷<paramref name="execute"/>是否可以執行的方法，需有一個<see cref="object"/>類別的參數。</param>
-    public RelayCommand(Action<ParameterType> execute, Predicate<ParameterType> canExecute) : this(execute)
+    public RelayCommand(Action<TParam> execute, Predicate<TParam> canExecute) : this(execute)
     {
       this._canExecute = canExecute;
     }
@@ -90,11 +71,51 @@ namespace CycWpfLibrary.MVVM
     /// <summary>
     /// 判斷<see cref="Execute(object)"/>是否可以執行。
     /// </summary>
-    public bool CanExecute(object parameter) => _canExecute is null || _canExecute((ParameterType)parameter);
+    public override bool CanExecute(object parameter) => _canExecute is null || _canExecute((TParam)parameter);
 
     /// <summary>
     /// 執行封裝的方法。
     /// </summary>
-    public void Execute(object parameter) => _execute((ParameterType)parameter);
+    public override void Execute(object parameter) => _execute((TParam)parameter);
+  }
+
+  /// <summary>
+  /// 將帶有參數及回傳值的方法封裝成命令的類別
+  /// </summary>
+  /// <typeparam name="TParam">參數的類型。</typeparam>
+  /// <typeparam name="TResult">回傳值的類型。</typeparam>
+  public class RelayCommand<TParam, TResult> : RelayCommandBase
+  {
+    private Predicate<TParam> _canExecute;
+    private Func<TParam, TResult> _execute;
+
+    /// <summary>
+    /// 將方法<paramref name="execute"/>封裝成命令，並使此命令永遠可執行。
+    /// </summary>
+    /// <param name="execute">要封裝成命令的方法，需有一個<see cref="TParam"/>類型的參數以及<typeparamref name="TResult"/>類型的回傳值。</param>
+    public RelayCommand(Func<TParam, TResult> execute)
+    {
+      this._execute = execute;
+      _canExecute = (obj) => true;
+    }
+    /// <summary>
+    /// 將方法<paramref name="execute"/>封裝成命令，並由<paramref name="canExecute"/>判斷此命令是否可執行。
+    /// </summary>
+    /// <param name="execute">要封裝成命令的方法，需有一個<see cref="TParam"/>類型的參數以及<typeparamref name="TResult"/>類型的回傳值。</param>
+    /// <param name="canExecute">判斷<paramref name="execute"/>是否可以執行的方法，需有一個<see cref="TParam"/>類別的參數。</param>
+    public RelayCommand(Func<TParam, TResult> execute, Predicate<TParam> canExecute) : this(execute)
+    {
+      this._canExecute = canExecute;
+    }
+
+    /// <summary>
+    /// 判斷<see cref="Execute(object)"/>是否可以執行。
+    /// </summary>
+    public override bool CanExecute(object parameter) => _canExecute is null || _canExecute((TParam)parameter);
+
+    /// <summary>
+    /// 執行封裝的方法。
+    /// </summary>
+    public override void Execute(object parameter) => _execute((TParam)parameter);
   }
 }
