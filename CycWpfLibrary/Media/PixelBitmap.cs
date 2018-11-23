fd75cb7c060ae9a65cd572d936a53577daf4b593
Alex Chuang <1392alex1392@gmail.com>
2018-11-23 01:48:39 +0000

    [PixelBitmap] Dispatcher.Invoke(Show()), Bgra SetPixel

--- CycWpfLibrary/Media/PixelBitmap.cs
+++ CycWpfLibrary/Media/PixelBitmap.cs
@@ -4,11 +4,13 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Windows;
 using System.Windows.Threading;
 using Colors = System.Windows.Media.Colors;
 using DependencyObject = System.Windows.DependencyObject;
 using Grid = System.Windows.Controls.Grid;
 using Image = System.Windows.Controls.Image;
+using Size = System.Drawing.Size;
 using SolidColorBrush = System.Windows.Media.SolidColorBrush;
 using Window = System.Windows.Window;
 
@@ -16,14 +18,15 @@ namespace CycWpfLibrary.Media
 {
   public class PixelBitmap : DependencyObject, ICloneable
   {
-    public int Width;
-    public int Height;
-    public Size Size;
-    public readonly int Byte = 4; //根據Format32bppArgb
-    public int Stride;  //根據Format32bppArgb
-    public static PixelFormat PixelImageFormat = PixelFormat.Format32bppArgb;
+    public readonly PixelFormat PixelFormat = PixelFormat.Format32bppArgb;
+    public readonly int Depth = 4; //根據Format32bppArgb
+    private readonly object key = new object();
 
-    private static readonly object key = new object();
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+    public Size Size { get; private set; }
+    public int Stride { get; private set; }  //根據Format32bppArgb
+
     private Bitmap bitmap;
     /// <summary>
     /// 自動設定<see cref="Width"/>, <see cref="Height"/>, <see cref="Size"/>, <see cref="Stride"/>等欄位。
@@ -37,18 +40,18 @@ namespace CycWpfLibrary.Media
         Width = _Bitmap.Width;
         Height = _Bitmap.Height;
         Size = _Bitmap.Size;
-        Stride = _Bitmap.Width * Byte;
+        Stride = _Bitmap.Width * Depth;
       }
     }
     /// <summary>
-    /// <see cref="System.Drawing.Bitmap"/>物件不可同時被多執行緒使用，需要使用互斥鎖<see cref="lock"/>，且getter需回傳副本。
+    /// Bitmap物件不可同時被多執行緒使用，需使用互斥鎖，getter回傳副本。
     /// </summary>
     public Bitmap Bitmap
     {
       get
       {
         Bitmap bitmap;
-        lock (key) 
+        lock (key)
         {
           bitmap = _Bitmap.Clone() as Bitmap;
         }
@@ -81,30 +84,27 @@ namespace CycWpfLibrary.Media
     }
     public PixelBitmap(Bitmap bitmap)
     {
-      NormalizeFormat();
+      // 注意! 24bpp轉成32bpp時，alpha值會被覆寫成255
+      if (bitmap.PixelFormat != PixelFormat)
+        bitmap = bitmap.SetPixelFormat(PixelFormat);
       Bitmap = new Bitmap(bitmap); //更新pixel
-
-      void NormalizeFormat()
-      {
-        if (bitmap.PixelFormat != PixelImageFormat)
-        {
-          var bitmapFormatted = new Bitmap(bitmap.Width, bitmap.Height, PixelImageFormat);
-          using (Graphics g = Graphics.FromImage(bitmapFormatted))
-          {
-            g.DrawImage(bitmap, new Rectangle(0, 0, bitmap.Width, bitmap.Height));
-          }
-          bitmap = bitmapFormatted;
-        }
-      }
     }
     public PixelBitmap(Size size)
     {
-      _Bitmap = new Bitmap(size.Width, size.Height, PixelImageFormat); //不更新pixel
+      _Bitmap = new Bitmap(size.Width, size.Height, PixelFormat); //不更新pixel
     }
     public PixelBitmap(byte[] pixel, Size size)
     {
-      _Bitmap = new Bitmap(size.Width, size.Height, PixelImageFormat); //不更新pixel
+      _Bitmap = new Bitmap(size.Width, size.Height, PixelFormat); //不更新pixel
       Pixel = pixel; //更新bitmap
+    }
+    /// <summary>
+    /// 將Emgu.Image.Data設定至PixelBitmap.Pixel
+    /// </summary>
+    /// <param name="bgraData"><see cref="Emgu"/>.<see cref="Image"/>.Data</param>
+    public PixelBitmap(byte[,,] bgraData)
+    {
+      SetPixel(bgraData);
     }
     public object Clone()
     {
@@ -122,62 +122,68 @@ namespace CycWpfLibrary.Media
     } //比 new 一個物件快十倍
 
     // public methods
-    public void ShowSnapShot()
+    public void Show()
     {
-      var window = new Window
+      Application.Current.Dispatcher.Invoke(() =>
       {
-        Background = new SolidColorBrush(Colors.LightGray),
-      };
-      var grid = new Grid
-      {
-        Background = null,
-      };
-      var image = new Image();
-      window.Content = grid;
-      grid.Children.Add(image);
-      image.Source = this.ToBitmapSource();
-      window.ShowDialog();
+        var window = new Window
+        {
+          Background = new SolidColorBrush(Colors.LightGray),
+        };
+        var grid = new Grid
+        {
+          Background = null,
+        };
+        var image = new Image();
+        window.Content = grid;
+        grid.Children.Add(image);
+        image.Source = this.ToBitmapSource();
+        window.ShowDialog();
+
+      });
     }
 
-    public byte[,,] GetPixel3()
+    /// <summary>
+    /// 取得圖像的三維陣列，像素深度按A,R,G,B排列
+    /// </summary>
+    public byte[,,] GetPixel3Argb()
     {
       var pixel = _Pixel.Clone() as byte[];
-      var width = Width;
-      var height = Height;
-      var pixel3 = new byte[width, height, Byte];
+      var pixel3 = new byte[Width, Height, Depth];
       int idx;
-      for (int x = 0; x < width; x++)
-      {
-        for (int y = 0; y < height; y++)
+      for (int x = 0; x < Width; x++)
+        for (int y = 0; y < Height; y++)
         {
-          idx = x * Byte + y * Stride;
-          pixel3[x, y, 3] = pixel[idx]; //B
+          idx = x * Depth + y * Stride;
+          pixel3[x, y, 3] = pixel[idx + 0]; //B
           pixel3[x, y, 2] = pixel[idx + 1]; //G
           pixel3[x, y, 1] = pixel[idx + 2]; //R
           pixel3[x, y, 0] = pixel[idx + 3]; //A
         }
-      }
       return pixel3;
     }
-    public void SetPixel(byte[,,] iptPixel3)
+    /// <summary>
+    /// 以三維陣列設定圖像像素陣列，像素深度按B,G,R,A排列
+    /// </summary>
+    public void SetPixel(byte[,,] bgraData)
     {
-      var pixel3 = iptPixel3.Clone() as byte[,,];
-      var width = pixel3.GetLength(0);
-      var height = pixel3.GetLength(1);
-      var depth = pixel3.GetLength(2);
-      var pixel = new byte[width * height * depth];
+      var pixel3 = bgraData.Clone() as byte[,,];
+      var height = pixel3.GetLength(0);
+      var width = pixel3.GetLength(1);
+      var depth = pixel3.GetLength(2); //iptImage depth
+      var stride = depth * width; //iptImage stride
+      _Bitmap = new Bitmap(width, height, PixelFormat); //不更新pixel
+      var pixel = new byte[width * height * Depth];
       int idx;
-      for (int x = 0; x < width; x++)
-      {
-        for (int y = 0; y < height; y++)
+      for (int row = 0; row < height; row++)
+        for (int col = 0; col < width; col++)
         {
-          idx = x * Byte + y * Stride;
-          pixel[idx] = pixel3[x, y, 3]; //B
-          pixel[idx + 1] = pixel3[x, y, 2]; //G
-          pixel[idx + 2] = pixel3[x, y, 1]; //R
-          pixel[idx + 3] = pixel3[x, y, 0]; //A
+          idx = col * Depth + row * Stride;
+          pixel[idx + 0] = pixel3[row, col, 0]; //B
+          pixel[idx + 1] = pixel3[row, col, 1]; //G
+          pixel[idx + 2] = pixel3[row, col, 2]; //R
+          pixel[idx + 3] = pixel3[row, col, 3]; //A
         }
-      }
       Pixel = pixel; // Update Bitmap
     }
 
@@ -188,7 +194,7 @@ namespace CycWpfLibrary.Media
       BitmapData bitmapData = bitmap.LockBits(
       new Rectangle(0, 0, Width, Height),
       ImageLockMode.ReadOnly,
-      PixelImageFormat);
+      PixelFormat);
 
       //複製pixel到bitmapData中
       Marshal.Copy(_Pixel, 0, bitmapData.Scan0, _Pixel.Length);
@@ -202,7 +208,7 @@ namespace CycWpfLibrary.Media
       BitmapData bitmapData = bitmap.LockBits(
         new Rectangle(0, 0, Width, Height),
         ImageLockMode.ReadOnly,
-        PixelImageFormat);
+        PixelFormat);
 
       //初始化pixel陣列，用來儲存所有像素的訊息
       _Pixel = new byte[bitmapData.Stride * Height];
