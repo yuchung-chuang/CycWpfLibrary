a4d42ba645372a392744db62a1bb45277b14309a
Alex Chuang <1392alex1392@gmail.com>
2019-01-16 01:13:56 +0000

    [ImageEraser] Update ImageSource when ImageChanged

--- CycWpfLibrary/Controls/ImageEraser.xaml.cs
+++ CycWpfLibrary/Controls/ImageEraser.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using CycWpfLibrary.Geometry;
 using CycWpfLibrary.WinForm;
+using System;
 
 namespace CycWpfLibrary.Controls
 {
@@ -23,7 +24,17 @@ namespace CycWpfLibrary.Controls
       mainGrid.DataContext = this;
     }
 
-    public static readonly DependencyProperty ImageProperty = DependencyProperty.Register(nameof(Image), typeof(Image<Bgra, byte>), typeof(ImageEraser));
+    public static readonly DependencyProperty ImageProperty = DependencyProperty.Register(
+      nameof(Image), 
+      typeof(Image<Bgra, byte>), 
+      typeof(ImageEraser),
+      new PropertyMetadata(ImageChanged));
+
+    private static void ImageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      (d as ImageEraser).OnPropertyChanged(nameof(ImageSource));
+    }
+
     public Image<Bgra, byte> Image
     {
       get => GetValue(ImageProperty) as Image<Bgra, byte>;
@@ -35,11 +46,35 @@ namespace CycWpfLibrary.Controls
     private Point mousePos;
     private ScaleTransform scale => panZoom.scale;
     private double eraserSize => 20 / scale.ScaleX;
+    private bool isEdit;
 
+    private void image_MouseEnter(object sender, MouseEventArgs e)
+    {
+      image.Cursor = null;
+      rectCursor.Visibility = Visibility.Visible;
+    }
+    private void image_MouseLeave(object sender, MouseEventArgs e)
+    {
+      Cursor = Cursors.Arrow;
+      rectCursor.Visibility = Visibility.Collapsed;
+    }
     private void image_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
     {
+      isEdit = false;
       image.CaptureMouse();
       image_MouseMove(sender, e);
+    }
+    private void image_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+    {
+      image.ReleaseMouseCapture();
+      if (isEdit)
+      {
+        Image = Image; //invoke twoway binding for Image
+      }
+    }
+    private void image_MouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      UpdateCursor();
     }
 
     private void image_MouseMove(object sender, MouseEventArgs e)
@@ -47,34 +82,11 @@ namespace CycWpfLibrary.Controls
       mousePos = e.GetPosition(image);
       if (e.RightButton == MouseButtonState.Pressed && image.IsMouseCaptured)
       {
-
         // Erase Image
         CvInvoke.Rectangle(Image, new Rect(mousePos.Minus(new Point(eraserSize / 2, eraserSize / 2)), new Vector(eraserSize, eraserSize)).ToWinForm(), Color.FromArgb(0, 0, 0, 0).ToMCvScalar(), -1);
         OnPropertyChanged(nameof(ImageSource));
-        OnPropertyChanged(nameof(Image));
+        isEdit = true;
       }
-      UpdateCursor();
-    }
-
-    private void image_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
-    {
-      image.ReleaseMouseCapture();
-    }
-
-    private void image_MouseEnter(object sender, MouseEventArgs e)
-    {
-      image.Cursor = null;
-      rectCursor.Visibility = Visibility.Visible;
-    }
-
-    private void image_MouseLeave(object sender, MouseEventArgs e)
-    {
-      Cursor = Cursors.Arrow;
-      rectCursor.Visibility = Visibility.Collapsed;
-    }
-
-    private void image_MouseWheel(object sender, MouseWheelEventArgs e)
-    {
       UpdateCursor();
     }
 
