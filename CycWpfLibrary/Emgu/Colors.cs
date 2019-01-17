dfb628d246192b4139f73d01bf0adc1074982e29
Alex Chuang <1392alex1392@gmail.com>
2019-01-17 04:41:27 +0000

    add Emgu Colors and Drawing

---
+++ CycWpfLibrary/Emgu/Colors.cs
@@ -0,0 +1,15 @@
+﻿using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycWpfLibrary.Emgu
+{
+  public static class Bgras
+  {
+    public static readonly Bgra Transparent = new Bgra(0, 0, 0, 0);
+
+  }
+}
---
+++ CycWpfLibrary/Emgu/Drawing.cs
@@ -0,0 +1,20 @@
+﻿using CycWpfLibrary.WinForm;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CycWpfLibrary.Emgu
+{
+  public static class Drawing
+  {
+    public static void EraseImage(this Image<Bgra, byte> image, Rect rect)
+    {
+      CvInvoke.Rectangle(image, rect.ToWinForm(), Bgras.Transparent.MCvScalar, -1);
+    }
+  }
+}
