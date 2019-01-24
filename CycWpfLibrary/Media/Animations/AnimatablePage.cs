b3d4380b06fd79e512ae00927ba05fef9f4d6827
Alex Chuang <1392alex1392@gmail.com>
2019-01-24 08:37:53 +0000

    Add animatablePage and AddPageAnimations

---
+++ CycWpfLibrary/Media/Animations/AnimatablePage.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace CycWpfLibrary.Media
+{
+  public enum PageSlideType
+  {
+    Left = 1,
+    Right = 2,
+    Top = 4,
+    Bottom = 8,
+  }
+  public enum PageTransitionType
+  {
+    In = 1,
+    Out = 2,
+  }
+  public enum PageAnimationType
+  {
+    None = 0,
+    Fade = 1,
+    Slide = 2,
+    FadeSlide = Fade | Slide,
+  }
+
+  public class AnimatablePage : Page
+  {
+    public AnimatablePage()
+    {
+      Dispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() => 
+      {
+        var width = ActualWidth;
+        var height = ActualHeight;
+        FromToPairs[(PageSlideType.Left, PageTransitionType.In)] =
+          (new Thickness(-width, 0, width, 0), new Thickness(0));
+        FromToPairs[(PageSlideType.Right, PageTransitionType.In)] =
+          (new Thickness(width, 0, -width, 0), new Thickness(0));
+        FromToPairs[(PageSlideType.Top, PageTransitionType.In)] =
+          (new Thickness(0, -height, 0, height), new Thickness(0));
+        FromToPairs[(PageSlideType.Bottom, PageTransitionType.In)] =
+          (new Thickness(0, height, 0, -height), new Thickness(0));
+        FromToPairs[(PageSlideType.Left, PageTransitionType.Out)] =
+          (new Thickness(0), new Thickness(-width, 0, width, 0));
+        FromToPairs[(PageSlideType.Right, PageTransitionType.Out)] =
+          (new Thickness(0), new Thickness(width, 0, -width, 0));
+        FromToPairs[(PageSlideType.Top, PageTransitionType.Out)] =
+          (new Thickness(0), new Thickness(0, -height, 0, height));
+        FromToPairs[(PageSlideType.Bottom, PageTransitionType.Out)] =
+          (new Thickness(0), new Thickness(0, height, 0, -height));
+      }));
+    }
+
+    public double AnimationTime { get; set; } = 1;
+
+    public PageAnimationType AnimationType { get; set; } = PageAnimationType.FadeSlide;
+    public PageTransitionType TransitionType { get; set; }
+    public PageSlideType SlideType { get; set; }
+
+    public readonly Dictionary<(PageSlideType, PageTransitionType), (Thickness From, Thickness To)> FromToPairs = new Dictionary<(PageSlideType, PageTransitionType), (Thickness From, Thickness To)>();
+
+    public void BeginPageAnimation()
+    {
+      var storyboard = new Storyboard();
+      storyboard.AddPageAnimation(this);
+      storyboard.Begin(this);
+      Task.Delay(TimeSpan.FromSeconds(AnimationTime));
+    }
+  }
+}
---
+++ CycWpfLibrary/Media/Animations/AnimationExtensions.cs
@@ -0,0 +1,99 @@
+﻿using CycWpfLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace CycWpfLibrary.Media
+{
+  public static class AnimationExtensions
+  {
+    /// <summary>
+    /// 針對<paramref name="animatable"/>執行泛型動畫。
+    /// </summary>
+    /// <typeparam name="PropertyType">執行動畫的屬性型別。</typeparam>
+    /// <param name="animatable">要執行動畫的個體。</param>
+    /// <param name="dp">要執行動畫的屬性。</param>
+    /// <param name="to">屬性改變的目標值。</param>
+    /// <param name="sec">動畫的時長。</param>
+    public static void AnimateTo<PropertyType>(this IAnimatable animatable, DependencyProperty dp, PropertyType to, double sec)
+    {
+      DependencyObject animation;
+      var duration = TimeSpan.FromSeconds(sec);
+      switch (to)
+      {
+        case double d:
+          animation = new DoubleAnimation(d, duration);
+          break;
+        case Color color:
+          animation = new ColorAnimation(color, duration);
+          break;
+        case Thickness thickness:
+          animation = new ThicknessAnimation(thickness, duration);
+          break;
+        default:
+          throw new NotSupportedException();
+      }
+      animatable.BeginAnimation(dp, animation as AnimationTimeline);
+    }
+
+    private static readonly double decelerationRatio = 0.9;
+    public static void AddPageSlide(this Storyboard storyboard, AnimatablePage page)
+    {
+      var (fromThickness, toThickness) = page.FromToPairs[(page.SlideType, page.TransitionType)];
+
+      var animation = new ThicknessAnimation
+      {
+        From = fromThickness,
+        To = toThickness,
+        Duration = TimeSpan.FromSeconds(page.AnimationTime),
+        DecelerationRatio = decelerationRatio,
+      };
+
+      Storyboard.SetTargetProperty(animation, new PropertyPath("Margin"));
+      storyboard.Children.Add(animation);
+    }
+
+    public static void AddPageFade(this Storyboard storyboard, AnimatablePage page)
+    {
+      double from = 0, to = 0;
+      switch (page.TransitionType)
+      {
+        case PageTransitionType.In:
+          from = 0;
+          to = 1;
+          break;
+        case PageTransitionType.Out:
+          from = 1;
+          to = 0;
+          break;
+        default:
+          break;
+      }
+
+      var animation = new DoubleAnimation
+      {
+        From = from,
+        To = to,
+        Duration = TimeSpan.FromSeconds(page.AnimationTime),
+        DecelerationRatio = decelerationRatio,
+      };
+
+      Storyboard.SetTargetProperty(animation, new PropertyPath("Opacity"));
+      storyboard.Children.Add(animation);
+    }
+
+    public static void AddPageAnimation(this Storyboard storyboard, AnimatablePage page)
+    {
+      if (page.AnimationType.Contain(PageAnimationType.Fade))
+        AddPageFade(storyboard, page);
+      if (page.AnimationType.Contain(PageAnimationType.Slide))
+        AddPageSlide(storyboard, page);
+    }
+  }
+}
