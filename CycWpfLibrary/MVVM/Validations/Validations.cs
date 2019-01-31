798f05952eb0e08d6618d59cf24f31e08d5ffac4
Alex Chuang <1392alex1392@gmail.com>
2019-01-31 00:55:36 +0000

    Add validations

--- CycWpfLibrary/MVVM/Validations/Validations.cs
+++ CycWpfLibrary/MVVM/Validations/Validations.cs
@@ -4,10 +4,16 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace CycWpfLibrary.MVVM
 {
+  public class NotNullValidation : ValidationRule
+  {
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo) => ValidationHelpers.IsNotNull(value);
+  }
+
   public class DoubleValidation : ValidationRule
   {
     public override ValidationResult Validate(object value, CultureInfo cultureInfo) => ValidationHelpers.IsDouble(value);
@@ -17,4 +23,33 @@ namespace CycWpfLibrary.MVVM
   {
     public override ValidationResult Validate(object value, CultureInfo cultureInfo) => ValidationHelpers.IsLogBase(value);
   }
+
+  public class ByteValidation : ValidationRule
+  {
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo) => ValidationHelpers.IsByte(value);
+  }
+
+  public class RangeValidation : ValidationRule
+  {
+    public int Maximum { get; set; } = int.MaxValue;
+    public int Minimum { get; set; } = int.MinValue;
+
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo) => ValidationHelpers.IsInRange(value, Maximum, Minimum);
+  }
+
+  public class DateValidationRule : ValidationRule
+  {
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo) => ValidationHelpers.IsDate(value);
+  }
+
+  public class FutureDateValidationRule : ValidationRule
+  {
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo) => ValidationHelpers.IsFutureDate(value);
+  }
+
+  public class PastDateValidationRule : ValidationRule
+  {
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo) => ValidationHelpers.IsPastDate(value);
+  }
+
 }
