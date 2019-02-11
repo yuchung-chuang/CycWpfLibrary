152e830d3f5cd8aec0037d9101202cc60cf840cc
Alex Chuang <1392alex1392@gmail.com>
2019-02-11 05:24:31 +0000

    Add VisibilityConverter

---
+++ CycWpfLibrary/CycWpfLibrary/WPF/ValueConverters/VisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CycWpfLibrary
+{
+  public class VisibilityConverter : ValueConverterBase<VisibilityConverter>
+  {
+    /// <summary>
+    /// Convert <see cref="bool"/> to <see cref="Visibility"/>
+    /// </summary>
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return (Visibility)value == Visibility.Visible ? true : false;
+    }
+  }
+}
