70dee5ddfda5b6f0d527ae5036660e5b9ea5808a
Alex Chuang <1392alex1392@gmail.com>
2019-03-10 16:50:49 +0000

    complete notify icon

--- CycWpfLibrary.FluentDesign/CustomControls/RevealWindow.cs
+++ CycWpfLibrary.FluentDesign/CustomControls/RevealWindow.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
 
 namespace CycWpfLibrary.FluentDesign
@@ -12,18 +14,32 @@ namespace CycWpfLibrary.FluentDesign
     }
 
     private NotifyIcon notifyIcon;
+    public ICommand NotifyIconCommand { get; set; }
 
     public RevealWindow()
     {
       this.notifyIcon = new NotifyIcon();
-      this.notifyIcon.Icon = (this.Icon as BitmapSource).ToBitmap().ToIcon();
+      this.Loaded += RevealWindow_Loaded;
       this.notifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;
+      NotifyIconCommand = new RelayCommand(MinimizeToNotifyIcon);
     }
 
-    private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+    private void RevealWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+      this.notifyIcon.Icon = (this.Icon as BitmapSource).ToBitmap().ToIcon();
+    }
+
+    private void MinimizeToNotifyIcon()
+    {
+      this.Hide();
+      notifyIcon.Visible = true;
+    }
+
+    private void NotifyIcon_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
     {
       this.Show();
       this.WindowState = WindowState.Normal;
+      notifyIcon.Visible = false;
     }
   }
 }
