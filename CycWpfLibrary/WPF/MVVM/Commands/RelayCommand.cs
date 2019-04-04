c81a4a127e93d48b4aaf82d6f7c66db25cc90fa4
Alex Chuang <1392alex1392@gmail.com>
2019-04-04 21:04:01 +0100

    Add Messenger to viewmodel

---
+++ CycWpfLibrary/WPF/MVVM/Commands/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CycWpfLibrary
+{
+  /// <summary>
+  /// 將<see cref="Action"/>封裝成命令的類別。
+  /// </summary>
+  public class RelayCommand : RelayCommandBase
+  {
+    private readonly Action _execute;
+    private readonly Func<bool> _canExecute;
+    /// <summary>
+    /// 將方法<paramref name="execute"/>封裝成命令，並使此命令永遠可執行。
+    /// </summary>
+    /// <param name="execute">要封裝成命令的方法。</param>
+    public RelayCommand(Action execute)
+    {
+      _execute = execute;
+      _canExecute = () => true;
+    }
+    /// <summary>
+    /// 將方法<paramref name="execute"/>封裝成命令，並由<paramref name="canExecute"/>判斷此命令是否可執行。
+    /// </summary>
+    /// <param name="execute">要封裝成命令的方法。</param>
+    /// <param name="canExecute">判斷<paramref name="action"/>是否可執行的方法。</param>
+    public RelayCommand(Action execute, Func<bool> canExecute) : this(execute)
+    {
+      _canExecute = canExecute;
+    }
+
+    /// <summary>
+    /// 判斷<see cref="Execute(object)"/>是否可以執行。
+    /// </summary>
+    public override bool CanExecute(object parameter = null) => _canExecute is null || _canExecute();
+
+    /// <summary>
+    /// 執行封裝的方法。
+    /// </summary>
+    public override void Execute(object parameter = null) => _execute();
+  }
+
+}
---
+++ CycWpfLibrary/WPF/MVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+
+namespace CycWpfLibrary
+{
+  /// <summary>
+  /// 提供ViewMoel的基底功能。
+  /// </summary>
+  public class ViewModelBase : ObservableObject, IViewValidation, ICleanup
+  {
+    private IMessenger _messengerInstance;
+    protected IMessenger MessengerInstance
+    {
+      get => _messengerInstance ?? Messenger.Default;
+      set => _messengerInstance = value;
+    }
+
+    public ViewModelBase()
+    {
+
+    }
+
+    public ViewModelBase(IMessenger messenger) : this()
+    {
+      MessengerInstance = messenger;
+    }
+
+    /// <summary>
+    /// Unregisters this instance from the Messenger class.
+    /// <para>To cleanup additional resources, override this method, clean
+    /// up and then call base.Cleanup().</para>
+    /// </summary>
+    public virtual void Cleanup()
+    {
+      MessengerInstance.Unregister(this);
+    }
+
+    /// <summary>
+    /// Dynamic expression
+    /// </summary>
+    public object this[string propertyName]
+    {
+      get
+      {
+        PropertyInfo property = GetType().GetProperty(propertyName);
+        return property.GetValue(this, null);
+      }
+      set
+      {
+        PropertyInfo property = GetType().GetProperty(propertyName);
+        property.SetValue(this, value, null);
+      }
+    }
+
+    public bool IsViewValid { get; set; }
+  }
+}
