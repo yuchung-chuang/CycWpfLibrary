7d5c0456bddc8ba0879b91494bb6cbc1e5ffd905
Alex Chuang <1392alex1392@gmail.com>
2019-04-07 04:22:08 +0100

    add iocContainer

---
+++ CycWpfLibrary/IoC/IocContainer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycWpfLibrary
+{
+  public class IocContainer : IocContainerBase
+  {
+    private readonly Dictionary<Type, Delegate> _factories = new Dictionary<Type, Delegate>();
+    private readonly Dictionary<Type, object> _instances = new Dictionary<Type, object>();
+
+    public readonly static IocContainer Default = new IocContainer();
+
+    public bool IsRegistered<TClass>()
+    {
+      var classType = typeof(TClass);
+      return _factories.ContainsKey(classType);
+    }
+
+    public bool IsCreated<TClass>()
+    {
+      var classType = typeof(TClass);
+      return _instances.ContainsKey(classType);
+    }
+
+    public void Register<TClass>()
+    {
+      var classType = typeof(TClass);
+      if (IsRegistered<TClass>()) // already registered
+        return;
+      else
+      {
+        Func<TClass> factory = NativeMethod.MakeInstance<TClass>;
+        _factories.Add(classType, factory);
+      }
+    }
+
+    public void Unregister<TClass>()
+    {
+      var classType = typeof(TClass);
+
+      _factories.Remove(classType); // return false if not found
+      _instances.Remove(classType);
+    }
+
+    public TClass Get<TClass>()where TClass : class
+    {
+      var classType = typeof(TClass);
+      if (!IsRegistered<TClass>())
+        throw new InvalidOperationException($"{classType} has not registered.");
+
+      if (!IsCreated<TClass>()) // not initiated
+      {
+        var instance = _factories[classType].DynamicInvoke(null);
+        _instances.Add(classType, instance);
+        return instance as TClass;
+      }
+      else // initiated
+      {
+        return _instances[classType] as TClass;
+      }
+    }
+
+    public void Reset()
+    {
+      _factories.Clear();
+      _instances.Clear();
+    }
+  }
+}
---
+++ CycWpfLibrary/IoC/IocContainerBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycWpfLibrary
+{
+  public interface IocContainerBase
+  {
+    bool IsRegistered<TClass>();
+    bool IsCreated<TClass>();
+    void Register<TClass>();
+    void Unregister<TClass>();
+    TClass Get<TClass>() where TClass : class;
+    void Reset();
+  }
+}
