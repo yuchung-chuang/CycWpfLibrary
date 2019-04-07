f3cf0e11ec96d336f74ba3dc0bbe59e4d287ff69
Alex Chuang <1392alex1392@gmail.com>
2019-04-07 04:23:10 +0100

    add eventMessage, modify message classes

---
+++ CycWpfLibrary/WPF/MVVM/Messaging/EventMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycWpfLibrary
+{
+  public class EventMessage<TArgs> : MessageBase
+    where TArgs : class
+  {
+    public EventMessage(TArgs args)
+    {
+      Args = args;
+    }
+
+    public TArgs Args { get; set; }
+  }
+}
---
+++ CycWpfLibrary/WPF/MVVM/Messaging/GenericMessage.cs
@@ -0,0 +1,54 @@
+﻿
+namespace CycWpfLibrary
+{
+  /// <summary>
+  /// Passes a generic value (Content) to a recipient.
+  /// </summary>
+  /// <typeparam name="T">The type of the Content property.</typeparam>
+  ////[ClassInfo(typeof(Messenger))]
+  public class Message<T> : MessageBase
+  {
+    /// <summary>
+    /// Initializes a new instance of the GenericMessage class.
+    /// </summary>
+    /// <param name="content">The message content.</param>
+    public Message(T content)
+    {
+      Content = content;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the GenericMessage class.
+    /// </summary>
+    /// <param name="sender">The message's sender.</param>
+    /// <param name="content">The message content.</param>
+    public Message(object sender, T content)
+        : base(sender)
+    {
+      Content = content;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the GenericMessage class.
+    /// </summary>
+    /// <param name="sender">The message's sender.</param>
+    /// <param name="target">The message's intended target. This parameter can be used
+    /// to give an indication as to whom the message was intended for. Of course
+    /// this is only an indication, amd may be null.</param>
+    /// <param name="content">The message content.</param>
+    public Message(object sender, object target, T content)
+        : base(sender, target)
+    {
+      Content = content;
+    }
+
+    /// <summary>
+    /// Gets or sets the message's content.
+    /// </summary>
+    public T Content
+    {
+      get;
+      protected set;
+    }
+  }
+}
