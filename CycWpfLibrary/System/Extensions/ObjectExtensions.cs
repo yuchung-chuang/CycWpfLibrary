2bc707efe641f8f91f2378fd09aead00f7318bdf
Alex Chuang <1392alex1392@gmail.com>
2019-04-07 04:19:29 +0100

    Get and Set property by class

--- CycWpfLibrary/System/Extensions/ObjectExtensions.cs
+++ CycWpfLibrary/System/Extensions/ObjectExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,13 +30,69 @@ namespace CycWpfLibrary
           return false;
       }
     }
-
     /// <summary>
     /// return empty string if object is null
     /// </summary>
     public static string ToStringEx(this object obj)
     {
       return (obj ?? "").ToString();
+    }
+
+    private static PropertyInfo GetPropInfo<TProperty>(this Type type)
+    {
+      return type.GetProperties()
+        .First(p => p.PropertyType == typeof(TProperty));
+    }
+    /// <summary>
+    /// Get property of <paramref name="obj"/> with <typeparamref name="TProperty"/>
+    /// </summary>
+    public static TProperty Get<TProperty>(this object obj)
+      where TProperty : class
+    {
+      return obj.GetType()
+        .GetPropInfo<TProperty>()
+        .GetValue(obj) as TProperty;
+    }
+    /// <summary>
+    /// Get property of <paramref name="type"/> with <typeparamref name="TProperty"/>
+    /// </summary>
+    /// <remarks>Use for static class or static method</remarks>
+    public static TProperty Get<TProperty>(this Type type)
+      where TProperty : class
+    {
+      return type.GetPropInfo<TProperty>()
+        .GetValue(null) as TProperty;
+    }
+
+    public static void Set<TProperty>(this object obj, TProperty value)
+    {
+      obj.GetType()
+        .GetPropInfo<TProperty>()
+        .SetValue(obj,value);
+    }
+    public static void Set<TProperty>(this Type type, TProperty value)
+      where TProperty : class
+    {
+      type.GetPropInfo<TProperty>()
+        .SetValue(null, value);
+    }
+
+
+    /// <summary>
+    /// Getter with dynamic expression
+    /// </summary>
+    public static object Get(this object obj, string propertyName)
+    {
+      PropertyInfo property = obj.GetType().GetProperty(propertyName);
+      return property.GetValue(obj);
+    }
+    /// <summary>
+    /// Setter with dynamic expression
+    /// </summary>
+    public static void Set(this object obj, string propertyName, object value)
+    {
+      PropertyInfo property = obj.GetType().GetProperty(propertyName);
+      property.SetValue(obj, value);
     }
   }
 }
