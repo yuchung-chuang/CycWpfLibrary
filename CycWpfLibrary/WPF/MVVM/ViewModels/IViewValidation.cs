63231de62bec3f25ae47d803a4b5339862ba8966
Alex Chuang <1392alex1392@gmail.com>
2019-04-07 04:23:33 +0100

    add IViewValidation

---
+++ CycWpfLibrary/WPF/MVVM/ViewModels/IViewValidation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycWpfLibrary
+{
+  public interface IViewValidation
+  {
+    bool IsViewValid { get; set; }
+  }
+}
--- CycWpfLibrary/WPF/MVVM/ViewModels/ViewModelBase.cs
+++ CycWpfLibrary/WPF/MVVM/ViewModels/ViewModelBase.cs
@@ -7,52 +7,8 @@ namespace CycWpfLibrary
   /// <summary>
   /// 提供ViewMoel的基底功能。
   /// </summary>
-  public class ViewModelBase : ObservableObject, IViewValidation, ICleanup
+  public class ViewModelBase : ObservableObject, IViewValidation
   {
-    private IMessenger _messengerInstance;
-    protected IMessenger MessengerInstance
-    {
-      get => _messengerInstance ?? Messenger.Default;
-      set => _messengerInstance = value;
-    }
-
-    public ViewModelBase()
-    {
-
-    }
-
-    public ViewModelBase(IMessenger messenger) : this()
-    {
-      MessengerInstance = messenger;
-    }
-
-    /// <summary>
-    /// Unregisters this instance from the Messenger class.
-    /// <para>To cleanup additional resources, override this method, clean
-    /// up and then call base.Cleanup().</para>
-    /// </summary>
-    public virtual void Cleanup()
-    {
-      MessengerInstance.Unregister(this);
-    }
-
-    /// <summary>
-    /// Dynamic expression
-    /// </summary>
-    public object this[string propertyName]
-    {
-      get
-      {
-        PropertyInfo property = GetType().GetProperty(propertyName);
-        return property.GetValue(this, null);
-      }
-      set
-      {
-        PropertyInfo property = GetType().GetProperty(propertyName);
-        property.SetValue(this, value, null);
-      }
-    }
-
     public bool IsViewValid { get; set; }
   }
 }
