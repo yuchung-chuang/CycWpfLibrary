d8f5943625e5f60031954138bbb14b4c5763bbd2
Alex Chuang <1392alex1392@gmail.com>
2019-04-07 04:21:24 +0100

    add staticpropertychanged event

---
+++ CycWpfLibrary/WPF/MVVM/ViewModels/StaticPropertyChangedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CycWpfLibrary
+{
+  public class StaticPropertyChangedEventArgs : EventArgs
+  {
+    public StaticPropertyChangedEventArgs(string className, string propertyName)
+    {
+      ClassName = className;
+      PropertyName = propertyName;
+    }
+
+    public string PropertyName { get; set; }
+    public string ClassName { get; set; }
+  }
+}
